5f97218fe22254883ef27b49d9f8ceb6738d21b6
Axel <axelgomez0903@gmail.com>
2026-07-23 15:39:13 -0500

    added door textures and disable jump

--- scenes/first_person_controller.gd
+++ scenes/first_person_controller.gd
@@ -34,8 +34,8 @@ func _physics_process(delta: float) -> void:
 	# -------------------------------------------
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
 	
 	if Input.is_action_just_pressed("left_mouse"):
 		animation_player.play("swing_01")
