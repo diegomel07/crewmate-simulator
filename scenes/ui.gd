6f34f06774435ae7b173bfdba05891967ce47605
Axel <axelgomez0903@gmail.com>
2026-07-23 16:46:17 -0500

    minor changes

--- scenes/ui.gd
+++ scenes/ui.gd
@@ -3,7 +3,8 @@ extends CanvasLayer
 @onready var task_label: Label = $TaskLabel
 @onready var final_warning: Label = $FinalWarning
 @onready var final_video: VideoStreamPlayer = $VideoStreamPlayer
-@onready var killer: MeshInstance3D = $BlenderModel
+@onready var killer: Node3D = $"amongus-final"
+@onready var task_complete_sound: AudioStreamPlayer = $Scream
 
 func _ready() -> void:
 	_update_label()
@@ -13,11 +14,12 @@ func _on_minigame_finished(_task_id: String, success: bool) -> void:
 	if success:
 		_update_label()
 		if MinigameManager.cant_minigames_completed == 10:
-			$BlenderModel/Area3D.monitoring = true
+			$"amongus-final/Area3D".monitoring = true
 			final_warning.visible = true
 			killer.visible = true
 			$"../DirectionalLight3D".light_color = Color.RED
 			$"../backgroundNoise".volume_db = 0
+			task_complete_sound.play()
 
 func _update_label() -> void:
 	task_label.text = "Tareas completadas: %d" % MinigameManager.cant_minigames_completed
